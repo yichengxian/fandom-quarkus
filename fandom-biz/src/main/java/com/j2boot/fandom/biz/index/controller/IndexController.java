91850c642fff43163a152cfe5daa51ddbf1f6bab
yichengxian <yichengxian@Tom.com>
2020-11-17 20:04:56 +0800

    add redis client

--- fandom-biz/src/main/java/com/j2boot/fandom/biz/index/controller/IndexController.java
+++ fandom-biz/src/main/java/com/j2boot/fandom/biz/index/controller/IndexController.java
@@ -2,17 +2,17 @@ package com.j2boot.fandom.biz.index.controller;
 
 import com.j2boot.fandom.biz.index.service.IndexService;
 import com.j2boot.fandom.common.result.CommonResult;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
 
 @RestController
 @RequestMapping("/")
 public class IndexController {
 
-    @Resource
+    @Autowired
     private IndexService indexService;
 
     @GetMapping("/")
